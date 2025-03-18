42383fb3a87838b46ec75fe825bcbe8838d63194
Oliwia Ciupinska <oliwiaciupinska1@gmail.com>
2025-03-18 01:01:45 +0100

    Zadania Lab 1, Lab 2 i Lab 3

--- Lab1_2/src/Main.java
+++ Lab1_2/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Witaj! Program umożliwia wyświetlanie rozwiązań zadań z Laboratoriów z programowania obiektowego.\nWybierz numer laboratorium, a potem numer zadania.");
-        System.out.println("Po wybraniu zadania, wyświetlona zostanie jego treść. Program pokaże, czego potrzebuje, a następnie przedstawi wynik zadania lub sam wynik, bez podawania szczegółowych danych.");
+        System.out.println("Witaj, program służy do pokazywania rozwiazań zadań z Labolatoriów z programowania obiektowego.\nWybierz numer labolatoriów, następnie numer zadania.");
 
         WyborLaboratorium run = new WyborLaboratorium();
         run.WyborLaboratorium();
--- Lab1_2/src/WyborLaboratorium.java
+++ Lab1_2/src/WyborLaboratorium.java
@@ -1,5 +1,6 @@
 import Lab1.Lab1Main;
 import Lab2.Lab2Main;
+import Lab3.Lab3Main;
 import java.util.Scanner;
 
 public class WyborLaboratorium {
@@ -7,16 +8,16 @@ public class WyborLaboratorium {
     int numer;
     Scanner scan = new Scanner(System.in);
         do {
-            System.out.println("Podaj numer Labolatoriow (1 lub 2)");
+            System.out.println("Podaj numer Labolatoriow (1, 2 lub 3)");
             while (!scan.hasNextInt()) { //warunek sprawdzajacy poprawnosc podanych danych w tym przypadku czy sa to inty
-                System.out.println("Wymagana liczba 1 lub 2.");
+                System.out.println("Wymagana liczba 1, 2 lub 3.");
                 scan.next();
             }
             numer = scan.nextInt();
-            if (numer < 1 || numer > 2) {
+            if (numer < 1 || numer > 3) {
                 System.out.println("Nie ma takiego zadania sprobuj ponownie");
             }
-        } while (numer < 1 || numer > 2);
+        } while (numer < 0 || numer > 4);
 
         switch (numer) {
             case 1:
@@ -26,6 +27,10 @@ public class WyborLaboratorium {
             case 2:
                 System.out.println("Zadania z Labolatoriów 2");
                 Lab2Main.RunLab2Main();
+                break;
+            case 3:
+                System.out.println("Zadania z Labolatoriów 3");
+                Lab3Main.RunLab3Main();
                 break;
             default:
                 System.out.println("Błąd");
